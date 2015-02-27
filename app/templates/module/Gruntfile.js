362c75b2244f83b39acfedaebc075557d24fabf1
Federico Liva <fede91it@gmail.com>
2015-02-27 09:22:03 +0100

    Added Grunt build file for the module.

---
+++ app/templates/module/Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+	var pkg = grunt.file.readJSON('package.json');
+
+	// Read version from the Joomla manifest
+	var manifest = grunt.file.read('source/' + pkg.name + '.xml');
+	var version = manifest.match(/<version>(.+?)<\/version>/)[1];
+
+	// Project configuration.
+	grunt.initConfig({
+		compress: {
+			options: {
+				archive: 'dist/' + pkg.name + '-' + version + '.tar.gz'
+			},
+			main   : {
+				expand: true,
+				cwd   : 'source/',
+				src   : ['**/*', '!**/sass/**', '!**/less/**'] // Exclude CSS preprocessors folders
+			}
+		}
+	});
+
+	// Load the tasks to run
+	grunt.loadNpmTasks('grunt-contrib-compress');
+
+	// Default task(s).
+	grunt.registerTask('default', ['compress']);
+};
