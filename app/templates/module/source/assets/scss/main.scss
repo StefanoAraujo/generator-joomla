30f591c10482ad7237e9e5e3159db4d5a0194599
Federico Liva <fede91it@gmail.com>
2015-03-04 17:17:11 +0100

    Added standard Joomla fields for cache and other things.

---
+++ app/templates/module/source/assets/scss/main.scss
@@ -0,0 +1,5 @@
+/**
+ * @package   <%= name %>.
+ * @author    <%= author %> <${ authorEmail }>
+ * @copyright Copyright (C) <%= year %> <%= author %>
+ */
--- app/templates/module/source/mod_foobar.php
+++ app/templates/module/source/mod_foobar.php
@@ -10,11 +10,16 @@ defined('_JEXEC') or die;
 // Load module helper
 include_once __DIR__ . '/helper.php';
 
-// Instantiate the helper
-$helper = new <%= mainClassName %>Helper($params);
+// Miscellaneous
+$base = JUri::root(true);
+$doc  = JFactory::getDocument();
 
 // Load something
-$something = $helper->getSomething();
+$something = <%= mainClassName %>::getSomething();
+
+// Add assets
+$doc->addStyleSheet($base . '/modules/<%= mainClassName %>/assets/css/main.css');
+$doc->addStyleSheet($base . '/modules/<%= mainClassName %>/assets/js/main.js');
 
 // Parse the template file
 require JModuleHelper::getLayoutPath('<%= name %>', $params->get('layout', 'default'));
