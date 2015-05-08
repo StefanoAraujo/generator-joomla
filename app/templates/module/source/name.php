ef5b8d27ab854b4d56629f9bc5e812f3224d127e
Federico Liva <fede91it@gmail.com>
2015-05-08 18:17:36 +0200

    Moved CSS and JS inclusion in the module layout. Better for Joomla overrides

--- app/templates/module/source/name.php
+++ app/templates/module/source/name.php
@@ -8,16 +8,7 @@ defined('_JEXEC') or die;
 // Load module helper
 include_once __DIR__ . '/helper.php';
 
-// Miscellaneous
-$base = JUri::root(true);
-$doc  = JFactory::getDocument();
-
-// Load something
-$something = mod<%= nameCamelcase %>Helper::getSomething();
-
-// Add assets
-$doc->addStyleSheet($base . '/modules/<%= name %>/assets/css/main.css');
-$doc->addStyleSheet($base . '/modules/<%= name %>/assets/js/main.js');
+$helper = new mod<%= nameCamelcase %>Helper($params);
 
 // Parse the template file
 require JModuleHelper::getLayoutPath('<%= name %>', $params->get('layout', 'default'));
--- app/templates/module/source/tmpl/default.php
+++ app/templates/module/source/tmpl/default.php
@@ -5,6 +5,8 @@
 
 defined('_JEXEC') or die;
 
+JHtml::stylesheet('modules/<%= name %>/assets/css/main.css');
+JHtml::script('modules/<%= name %>/assets/js/main.js');
 ?>
 
 <div class="<%= _.slugify(name) %>">
